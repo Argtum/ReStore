87d3217b3ff867142480b22b10b575ae2ae968e2
Argtum <silwar40k@gmail.com>
2021-06-05 21:43:38 +0300

    Разделил ображение и обработку данных в компоненте book-list

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -8,7 +8,21 @@ import {compose} from "../../utils";
 import Spinner from "../spiner";
 import ErrorIndicator from "../error-indicator";
 
-class BookList extends Component {
+const BookList = ({books}) => {
+  return (
+    <ul className="book-list">
+      {
+        books.map((book) => {
+          return (
+              <li key={book.id}><BookListItem book={book}/></li>
+          )
+        })
+      }
+    </ul>
+  );
+};
+
+class BookListContainer extends Component {
 
   componentDidMount() {
     this.props.fetchBooks();
@@ -25,17 +39,7 @@ class BookList extends Component {
       return <ErrorIndicator />
     }
 
-    return (
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return (
-              <li key={book.id}><BookListItem book={book}/></li>
-            )
-          })
-        }
-      </ul>
-    );
+    return <BookList books={books} />
   }
 }
 
@@ -52,4 +56,4 @@ const mapDispatchToProps = (dispatch, {bookstoreService}) => {
 export default compose(
     withBookstoreService(),
     connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
