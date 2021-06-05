f896637771415cd13cf2729ae6f1ee36e9d84a88
Argtum <silwar40k@gmail.com>
2021-06-05 21:08:15 +0300

    Добавил обрботку ошибки при запросе

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,25 +3,31 @@ import "./book-list.css";
 import {BookListItem} from "../book-list-item";
 import {connect} from "react-redux";
 import withBookstoreService from "../hoc/with-bookstore-service";
-import {booksLoaded, booksRequested} from "../../actions";
+import {booksLoaded, booksRequested, booksError} from "../../actions";
 import {compose} from "../../utils";
 import Spinner from "../spiner";
+import ErrorIndicator from "../error-indicator";
 
 class BookList extends Component {
 
   componentDidMount() {
-    const {bookstoreService, booksLoaded, booksRequested} = this.props;
+    const {bookstoreService, booksLoaded, booksRequested, booksError} = this.props;
     booksRequested();
 
     bookstoreService.getBooks()
-        .then((data) => booksLoaded(data));
+        .then((data) => booksLoaded(data))
+        .catch((err) => booksError(err));
   }
 
   render() {
-    const {books, loading} = this.props;
+    const {books, loading, error} = this.props;
 
     if (loading) {
       return <Spinner />
+    }
+
+    if (error) {
+      return <ErrorIndicator />
     }
 
     return (
@@ -38,13 +44,14 @@ class BookList extends Component {
   }
 }
 
-const mapStateToProps = ({books, loading}) => {
-  return {books, loading};
+const mapStateToProps = ({books, loading, error}) => {
+  return {books, loading, error};
 };
 
 const mapDispatchToProps = {
   booksLoaded,
-  booksRequested
+  booksRequested,
+  booksError
 };
 
 export default compose(
