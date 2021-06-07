bf3e6974da47c09ff1e59200057c30613da06da6
Argtum <silwar40k@gmail.com>
2021-06-07 08:33:27 +0300

    Практика enhance и middleware

--- src/store.js
+++ src/store.js
@@ -1,6 +1,24 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import reducer from "./reducers"
 
-const store = createStore(reducer);
+const logMiddleware = ({getState, dispatch}) => (dispatch) => (action) => {
+    console.log(action.type);
+
+    return dispatch(action);
+}
+
+const stringMiddleware = (state) => (dispatch) => (action) => {
+    if(typeof action === `string`) {
+        return dispatch({
+            type: action
+        })
+    }
+
+    return dispatch(action);
+}
+
+const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+
+store.dispatch(`HELLO_WORLD`);
 
 export default store;
